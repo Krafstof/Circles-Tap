3242fbd538e1d3147eb8522bfde69b205a69b2ef
moonlightMoth <ghjpa@inbox.ru>
2017-06-06 13:51:00 +0300

    Add files via upload

---
+++ mrtap/Circle.java
@@ -0,0 +1,39 @@
+package com.example.user.mrtap;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+public class Circle extends Button {
+
+    public Circle(Context con){
+        super(con);
+    }
+
+    public void setupDrawing()
+    {
+
+    }
+
+    public void createCircle(){
+
+    }
+
+    View.OnClickListener l = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    public void delete(){
+
+    }
+
+
+
+}
